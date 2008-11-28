3e7dc6bd8a1211674d7d2731d2c033c22b137472
buttock.scout <buttock.scout@d0e20bd2-bc82-11dd-becf-13071112e4e2>
2008-11-28 09:17:12 +0000

    Modified the build script to run from the terminal.

--- trunk/build.sh
+++ trunk/build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-cd "/Users/jonasvanderaa/school/Gestructureerd programmeren/programmeeropdracht/" &&
-gcc -Os -o programmeeropdracht main.c -lpanel -lmenu -lncurses_g -L/sw/lib/ncurses/;
+gcc -g -Os -o programmeeropdracht main.c -lpanel -lmenu -lncurses_g -L/sw/lib/ncurses/ && echo "Build done";
