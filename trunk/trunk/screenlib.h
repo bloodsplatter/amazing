de0abf6b1879c9681c0a7c6b7668c5b186317d79
buttock.scout <buttock.scout@d0e20bd2-bc82-11dd-becf-13071112e4e2>
2008-12-02 17:09:45 +0000

    attemt to restructure the svn tree (attempt 2)

---
+++ trunk/trunk/screenlib.h
@@ -0,0 +1,66 @@
+#include <ncurses.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+
+#define ENTER 10
+#define ESCAPE 27
+
+
+// declaraties
+bool color_support = 0;
+int curr_char = -1; // de laatst ingedrukte toets
+int loop = 1; // een flag om de eventloop te doorbreken
+static WINDOW *mainwnd; // het hoofdscherm (achtergrond)
+static WINDOW *display; // het huidige scherm
+enum DisplayMode
+{
+	start,
+	speelveld,
+	levelselectie,
+	levelbewerker
+};
+int DISPLAYMODE = start; // display mode switch
+bool DEBUG = TRUE; // een flag om debug mode te achterhalen, wordt ingesteld als de 1e parameter "debug" is
+
+void screen_init(void) {
+	// omgeving instellen
+	mainwnd = initscr(); // hoofdscher
+	noecho(); // characters niet printen
+	cbreak(); // direct characters doorgeven (geen buffer gebruiken)
+	nodelay(mainwnd, TRUE);
+	noraw();
+	intrflush(mainwnd,TRUE);
+	keypad(mainwnd, TRUE); // naar dit venster keys sturen
+	curs_set(0); // cursor onzichtbaar maken
+	color_support = has_colors(); // controleer of kleuren ondersteunt zijn
+	
+	// schermen tekenen;
+	refresh();
+}
+
+void free_res(void)
+{
+	// resources vrijgeven
+	curs_set(1);
+	echo();
+	nocbreak();
+}
+
+static void draw_loop(void)
+{
+	// schermen tekenen
+	refresh();
+}
+
+void color_init(void)
+{
+	if (color_support) // capaciteiten controleren
+	    {
+	        start_color();
+			// kleurenkoppels voor menu's
+		    init_pair(1,COLOR_WHITE,COLOR_BLUE);
+		    init_pair(2,COLOR_BLUE,COLOR_WHITE);
+		    init_pair(3,COLOR_RED,COLOR_WHITE);
+	    }
+}
