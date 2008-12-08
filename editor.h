79b7dc4439523eb26e74c8f21a66dd0f111d1791
buttock.scout <buttock.scout@d0e20bd2-bc82-11dd-becf-13071112e4e2>
2008-12-08 16:14:39 +0000

    Level selection and editing comming along nicely. The code to load and display a level is in place, now on to creating the actual levels and implementing a scoring system

--- editor.h
+++ editor.h
@@ -1,9 +1,75 @@
-// header file for leveleditor
-// should be included after playfield.h
-#ifndef PLAYFIELD
+// header file voor leveleditor
+// afhankelijkheden: playfield.h
+#ifndef _PLAYFIELD_
 #include "playfield.h"
 #endif
-#ifndef EDITOR
-#define EDITOR
+#ifndef _EDITOR_
+#define _EDITOR_
 #endif
 
+// functieprototypes
+void setStartPosition(int,int);
+void setEndPosition(int,int);
+void setWall(int, int);
+
+
+/*
+	het huidige venster staat in de display variabele
+	het huidige speelveld staat in de playfield variabele
+*/
+
+void setStartPosition(int y, int x) // stel het beginpunt in
+{
+	if (playfield.hasStart == FALSE) // er is nog geen startpunt
+	{
+		playfield.startPos.x = x;
+		playfield.startPos.y = y;
+		playfield.field_data[x][y] = PLAYER;
+		playfield.hasStart = TRUE;
+	}
+	else // verwissel startpunt
+	{
+		playfield.field_data[playfield.startPos.x][playfield.startPos.y] = WALL;
+		playfield.startPos.x = x;
+		playfield.startPos.y = y;
+		playfield.field_data[x][y] = PLAYER;
+	}
+}
+
+void setEndPosition(int y,int x) // stel eindpunt in
+{
+	if (playfield.hasFinish == FALSE) // er is nog geen eindpunt
+	{
+		playfield.endPos.x = x;
+		playfield.endPos.y = y;
+		playfield.field_data[x][y] = ENDPOINT;
+		playfield.hasFinish = TRUE;
+	}
+	else // verwissel eindpunt
+	{
+		playfield.field_data[playfield.endPos.x][playfield.endPos.y] = WALL;
+		playfield.endPos.x = x;
+		playfield.endPos.y = y;
+		playfield.field_data[x][y] = ENDPOINT;
+	}
+}
+
+void setWall(int y, int x) // zet muur op huidige plaats
+{
+	char *curPos = &playfield.field_data[x][y];
+	if (*curPos == WALL)
+	{
+		*curPos = EMPTY;
+	} else if (*curPos == EMPTY)
+	{
+		*curPos = WALL;
+	} else if (*curPos == ENDPOINT)
+	{
+		playfield.hasFinish = FALSE;
+		*curPos = WALL;
+	} else if (*curPos == PLAYER)
+	{
+		playfield.hasStart = FALSE;
+		*curPos = WALL;
+	}
+}
--- playfield.h
+++ playfield.h
@@ -1,18 +1,16 @@
+// header file voor het speelveld en de speelmodus
+// afhankelijkheden: screenlib.h
 // constanten
-#ifndef PLAYFIELD
-#define PLAYFIELD
+#ifndef _PLAYFIELD_
+#define _PLAYFIELD_
 #define WALL ACS_BLOCK
 #define PLAYER ACS_BULLET
 #define ENDPOINT ACS_CKBOARD
+#define EMPTY 0
 #endif
-#ifndef SCREENLIB
+#ifndef _SCREENLIB_
 #include "screenlib.h"
 #endif
-
-// functieprototypes
-void load_playfield(int);
-void write_level_list(char*);
-void load_level_list(char*);
 
 // definities
 struct Position
@@ -20,37 +18,57 @@ struct Position
 	int x;
 	int y;
 } playerPos;
+
 struct Playfield
 {
-	char naam[20];
-	int width;
-	int height;
-	WINDOW *win;
-	char field_data[40][20];
-	bool hasFinish;
-	bool hasStart;
+	char naam[20]; // naam van de level
+	int width; // breedte
+	int height; // hoogte
+	WINDOW *win; // venster (weergave)
+	char field_data[20][40]; // map data
+	bool hasFinish; // heeft finish
+	bool hasStart; // heeft start
+	struct Position startPos; // startpositie
+	struct Position endPos;
 } playfield, *levels; // datastructure met weergaveinfo over het speelveld
 
+int levelcount = 0;
+// functieprototypes
+void load_playfield(const int*);
+void write_level_list(char*);
+void load_level_list(char*);
+void draw_field(void);
+bool validate_playfield(const struct Playfield);
+
 // code om het speelveld te laden
-void load_playfield(int fieldnum)
+void load_playfield(const int* fieldnum)
 {
-	// voor vierkant: hoogte = breedte / 2
-	playfield.width = 40; playfield.height = 20;
-	playfield.win = newwin(playfield.height,playfield.width,LINES/2-playfield.height,COLS/2-playfield.height); // maak nieuw venster
+	if (fieldnum == NULL) {
+		// voor vierkant: hoogte = breedte / 2
+		playfield.width = 40; playfield.height = 20;
+		playfield.startPos.x = 0;
+		playfield.startPos.y = 0;
+		playfield.endPos.x = playfield.width - 1;
+		playfield.endPos.y = playfield.height - 1;
+		playfield.hasFinish = TRUE;
+		playfield.hasStart = TRUE;
+		playfield.win = newwin(playfield.height,playfield.width,LINES/2-playfield.height,COLS/2-playfield.height); // maak nieuw venster
+	} else {
+		memcpy(&playfield,&levels[*fieldnum],sizeof(playfield)); // kopieer speelveld uit array naar plaats voor huidig speelveld
+	}
 	display = dupwin(playfield.win); // kopieer het venster naar de tekenbuffer
 	load_commandwindow(); // laad het shortcutvenster opnieuw in
-	DISPLAYMODE = speelveld;
+	DISPLAYMODE = speelveld; // zet displaymodus
 }
 
 void write_level_list(char* naam) // schrijf de levels weg in een bestand
 {
-	naam = (naam == NULL)?"levels":naam;
-	int count = sizeof(levels)/sizeof(playfield); // het aantal levels
+	naam = (naam == NULL)?"levels":naam; // optionele argumenten initialiseren
 	FILE *file = NULL; // maak bestandspointer
 	file = fopen("levels","w"); // open het bestand voor schrijven
 	if (file != NULL) { // als het openen geslaagd is:
-		fprintf(file,"%d\n",count); // schrijf het aantal weg
-		fwrite(levels,sizeof(playfield),sizeof(levels)/sizeof(playfield),file); // schrijf de levels array weg
+		fprintf(file,"%d\n",levelcount); // schrijf het aantal weg
+		fwrite(levels,sizeof(playfield),levelcount,file); // schrijf de levels array weg
 		fclose(file); // sluit het bestand
 	}
 }
@@ -58,15 +76,38 @@ void write_level_list(char* naam) // schrijf de levels weg in een bestand
 // laad de levellijst in
 void load_level_list(char* naam)
 {
-	naam = (naam == NULL)?"levels":naam;
-	int count = 0; // initialisaties
+	naam = (naam == NULL)?"levels":naam; // optionele argumenten initialiseren
 	FILE *file = NULL; // maak bestandspointer
 	file = fopen("levels","r"); // open bestand voor lezen
 	if (file != NULL) // als het bestand geopend is:
 	{
-		fscanf(file,"%d%c",&count); // lees aantal levels in bestand
-		levels = (struct Playfield *)calloc(count,sizeof(playfield));
-		fread(levels,sizeof(playfield),sizeof(levels)/sizeof(playfield),file);
-		fclose(file);
+		fscanf(file,"%d",&levelcount); // lees aantal levels in bestand
+		if (levelcount>0) { // geen levels inlezen als deze er niet zijn
+			free(levels); // huidig level geheugen vrijgeven om memory leaks te voorkomen
+			levels = (struct Playfield *)calloc(levelcount,sizeof(playfield)); // maak genoeg geheugen vrij
+			fread(levels,sizeof(playfield),levelcount,file); // levels inlezen
+		}
+		fclose(file); // bestand sluiten
 	}
+}
+
+void draw_field(void) // teken het veld
+{
+	int r, c; // tellers
+	for (r=0;r<playfield.width;r++) // rijen aflopen
+	{
+		wclear(display); // wis de huidige inhoud
+		wmove(display,r,0); // zet de cursor aan het begin van de rij
+		for (c=0;c<playfield.height;c++) // kolommen aflopen
+		{
+			if (playfield.field_data[r][c] == WALL || playfield.field_data[r][c] == ENDPOINT) // alleen muren en het eindpunt tekenen
+				mvwaddch(display,r,c,playfield.field_data[r][c]);
+		}
+	}
+	mvwaddch(display,playerPos.y,playerPos.x,PLAYER); // teken de spelerpositie
+}
+
+bool validate_playfield(const struct Playfield speelveld)
+{
+	return speelveld.hasFinish && speelveld.hasStart;
 }
