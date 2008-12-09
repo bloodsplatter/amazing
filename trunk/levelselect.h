0a427f848b0f17b51cb4155ec2d8f70c8bbb0e11
buttock.scout <buttock.scout@d0e20bd2-bc82-11dd-becf-13071112e4e2>
2008-12-09 08:33:01 +0000

    numerous updates and the adition of the 7z LZMA compression algorithm. It is compiled from the publicly available SDK as a static library.

--- trunk/levelselect.h
+++ trunk/levelselect.h
@@ -15,11 +15,11 @@
 #endif
 
 // functieprototypes
-void load_select_window(void);
-int set_current_choice(int);
+void load_select_window(bool);
 void print_choices(void);
 void mvselection_down(void);
 void mvselection_up(void);
+void level_selected(void);
 
 // declaraties
 static WINDOW* menuselect;
@@ -34,31 +34,49 @@ void load_select_window(bool edit) // laad het selectiescherm
 		menuselect = newwin(LINES/2-3,COLS/2-COLS/4,LINES/4,(COLS/2-COLS/4)/2); // maak een gecentreerd venster
 		display = dupwin(menuselect); // kopieer venster
 	}
-	forEditing = edit;
+	forEditing = edit; // set the editing boolean
 	DISPLAYMODE = levelselectie; // zet displaymodus
-}
-
-int set_current_choice(int choice) // stel het huidige geselecteerde item in
-{
-	return choice = choice < 0?0:choice;
+	load_commandwindow();
 }
 
 void print_choices(void) // zet alle keuzes in het scherm
 {
-	wclear(display);
 	int i = 0;
-	for (;i<=levelcount;i++)
+	for (;i<=(forEditing?levelcount+1:levelcount);i++)
 	{
-		mvwprintw(display,i,1,"%c%d)%s",i==current_choice?SELECTOR:0x32,i,levels[i].naam);
+		mvwprintw(display,i,1,"%c%d)%s",i==current_choice?SELECTOR:0x32,i,levels[i].naam); // print alle namen van levels en de selectiecursor
+	}
+	if (forEditing == TRUE)
+	{
+		mvwprintw(display,i,1,"%c%d)%s",i==current_choice?SELECTOR:0x32,i+1,"nieuwe level"); // print ook een nieuwe entry om een level bij te maken
 	}
 }
 
 void mvselection_down(void) // stel de selectie 1 lager in of ga naar boven als we onderaan zijn
 {
-	current_choice = (current_choice+1>levelcount)?0:current_choice+1;
+	wclear(display);
+	current_choice = (current_choice+1>(forEditing?levelcount+1:levelcount))?0:current_choice+1;
 }
 
 void mvselection_up(void) // stel de selectie 1 hoger in of ga naar beneden als we bovenaan zijn
 {
-	current_choice = (current_choice-1<0)?levelcount:current_choice-1;
+	wclear(display);
+	current_choice = (current_choice-1<0)?(forEditing?levelcount+1:levelcount):current_choice-1;
+}
+
+void level_selected(void) // laat geselecteerde level
+{
+	if (current_choice > levelcount && forEditing) // maak nieuwe level aan
+	{
+		
+	} else // open geselecteerde level
+	{
+		if (forEditing)
+		{
+			// laad levelbewerker met huidige level in
+		} else 
+		{
+			// start huidige level
+		}
+	}
 }
--- trunk/tekst.h
+++ trunk/tekst.h
@@ -6,7 +6,7 @@ const char GAME_TITLE[] = "aMAZing"; // de titel die in de vensters weergeven wo
 const char MODE_0_S[3][150] = {"Shift + N: nieuw spel",
 							"Shift + Q: afsluiten",
 							"Shift + C: levelbewerker"}; // tekst van de zijbalk (MODUS start)
-const char MODE_1_S[2][150] = {"Shift + Q: afsluiten",
+const char MODE_1_S[2][150] = {"Shift + Q: naar hoofdmenu",
 								"Pijltjes: bewegen"}; // tekst van de zijbalk (MODUS speelveld)
 const char MODE_2_S[6][150] = {"Shift + S: Opslaan",
 								"Shift + Q: naar Hoofdmenu",
@@ -14,3 +14,6 @@ const char MODE_2_S[6][150] = {"Shift + S: Opslaan",
 								"W: muur aan/afzetten",
 								"X: einde plaatsen",
 								"B: begin plaatsen"}; // tekst van de zijbalk (MODUS levelbewerker)
+const char MODE_3_S[3][150] = {"Shift + Q: naar hoofdmenu",
+								"Pijltoetsen: level kiezen",
+								"Enter: levelkeuze bevestigen"}; // tekst van de zijbalk (MODUS levelselectie)
